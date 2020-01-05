24fc8ab34354ff0bbc1aadb2c2a15939f9799bcb
root <24620094+IAmBlackHacker@users.noreply.github.com>
2020-01-05 09:06:47 -0500

    added zsh setup

---
+++ ZSH Setup/zsh_setup.sh
@@ -0,0 +1,9 @@
+apt-get install figlet
+apt-get install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+cp .zshrc ~/
+
+echo "----------------------------------------------"
+echo "Setup Done!"
+echo "use: chsh (to change your shell)."
